86c66e6f7973657c83e946a2c4e81be4728ed610
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-07-02 12:45:42 +0530

    add cognito

--- kkk.js
+++ kkk.js
@@ -1,4 +1,17 @@
-exports.handler = function(request, response) {
-    
-    response.send({"message": "Successfully executed"});
+const AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = function (request, response) {
+    cognito_idp.listUsers({
+        UserPoolId: "us-east-1_HdYJb7Znp",
+        Limit: 10
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
+    response.send({ "message": "Successfully executed" });
 }
